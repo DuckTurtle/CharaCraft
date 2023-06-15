c592c92f0413c9232953b62e5cc07e430dcc9f9b
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-06-15 18:35:44 -0500

    tweaked signup and login.

--- public/js/login.js
+++ public/js/login.js
@@ -1,41 +1,30 @@
-const mysql = require('mysql')
-const express = require ('express');
-const session = require ('express-session');
-const path = require('path');
 
-
-const app = express
-
-// login route for user
-app.route('/login')
-    .get((req, res) => {
-        res.render('login', loginContent);        
-    })
-    .post((req, res)) => {
-        var username = req.body.username;
-        var password = req.body.password;
+//login for user
+function login(username, password) {
+    const requestBody = {
+        username: username,
+        password: password,
     }
+}
 
-    User.findOne({where: { username: username} }).then(function (user) {
-        if(!user) {
-            res.redirect('/login');
-        } else if (!user.validPassword(password)) {
-            res.redirect('/login')
-        } else {
-            req.session.user.= user.dataValues;
-            res.direct('/userPortal')
-        }
-    });
+fetch('api/dnd/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'}
+    },
+    body: JSON.stringify({email, password}),
+)
+.then(response => response.json())
+.then(data => {
 
-// UserPortal route 
-    app.get('/userPortal', (req,res) => {
-        if (req.session.user && req.cookies.user_id) {
-            loginContent.loggedin = true;
-            loginContent.username = req.sessions.user.username;
-            loginContent.title = "You are offically logged in!"
-            
-            res.render('userPortal', loginContent);
-        } else {
-            res.redirect('/login');
-        }
-        });
+if (data.sucess) {
+
+} else {
+
+}
+})
+.catch(error => {
+    console.error('Error:', error)
+    alert('Failed to login.')
+})
+
