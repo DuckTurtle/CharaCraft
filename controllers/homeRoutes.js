b5618bee72b76bca4f4919f6b27e8427b4e793c2
Jayson Baker <bakej07@gmail.com>
2023-06-15 21:23:45 -0500

    Fixed landing background and the homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
-const { Prebuilts } = require("../models");
+const {
+  Prebuilts,
+  Characters,
+  Weapon,
+  Spells,
+  User,
+  CharOther,
+  CharWeapon,
+  Charspell,
+  Other,
+} = require("../models");
+const checkAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -25,6 +36,65 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+router.get("/userPortal", checkAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const characterData = await Characters.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    // Serialize data so the template can read it
+    const character = characterData.map((char) => char.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("UserPortal", {
+      character,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/character", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const characterData = await Characters.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Spells,
+          through: Charspell,
+        },
+        {
+          model: Other,
+          through: CharOther,
+        },
+        {
+          model: Weapon,
+          through: CharWeapon,
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const character = characterData.map((char) => char.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("characterSheet", {
+      character,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
